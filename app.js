df4269364da49c1d3cd1ac397daf87c1cb83312d
koichirofunami <koichirofunami@192.168.150.246>
2018-06-03 19:21:53 +0900

    added fork gridCalcurator

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var child_process = require('child_process');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,6 +21,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//20180604　子プロセスとしてgirdCalcuratorを起動する
+var child = child_process.fork('./public/js/gridCalcurator.js');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
